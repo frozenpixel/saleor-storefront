43fe0eb9ea6323a60441ac863b9daac77249f680
frozenpixel <ashley.malone@gmail.com>
2020-08-14 19:24:16 -0500

    added dump to test merge config

--- config/webpack/config.prod.js
+++ config/webpack/config.prod.js
@@ -1,4 +1,19 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { WebpackConfigDumpPlugin } = require("webpack-config-dump-plugin");
+
+const options = {
+  // Path to store config dump
+  outputPath: './config',
+
+  // Config dump filename
+  name: 'webpack.config.dump',
+
+  // Config depth. Since webpack config is circulary locked,
+  // we can't dump whole config. This parameter sets how deep
+  // config dump will be stored
+  depth: 8
+
+};
 
 module.exports = ({ sourceDir, distDir }) => ({
   output: {
@@ -19,13 +34,11 @@ module.exports = ({ sourceDir, distDir }) => ({
       }
     ]
   },
-  resolve: { 
-    extensions: ['.ts', '.tsx', '.js', '.jsx'] 
-  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "[name].[hash].css",
       chunkFilename: "[id].[hash].css"
-    })
+    }),
+    new WebpackConfigDumpPlugin(options),
   ]
 });
