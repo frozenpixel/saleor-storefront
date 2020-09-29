0c90dda2974cde13ebc7430f6f7224f0d2797380
Ashley Malone <ashley.malone@gmail.com>
2020-09-29 15:28:21 -0500

    updated footer styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,8 +13,18 @@ const Footer: React.FC = () => (
       {SOCIAL_MEDIA.map(medium => (
         <SocialMediaIcon medium={medium} key={medium.ariaLabel} />
       ))}
+      <a href="//www.securitymetrics.com/site_certificate?id=285679&tk=a4d8b3616f1a0ffd00a37df5ff975299"
+        target="_blank" rel="noopener noreferrer">
+        <img src="https://www.securitymetrics.com/static/apps/super/assets/img/Credit_Card_Safe_light.png"
+          alt="SecurityMetrics Credit Card Safe" />
+      </a>
     </div>
     <Nav />
+    <footer className="footer__copyright">
+      <div className="container">
+        <span>© Copyright 2001-2020 Celletech, Ltd.</span>
+      </div>
+    </footer>
   </div>
 );
 
